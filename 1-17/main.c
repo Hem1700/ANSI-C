495e507693bd79fdf69e75907343cf67baf1d99c
Hem Parekh <54116609+Hem1700@users.noreply.github.com>
2021-02-05 09:17:42 +0530

    1-15,1-17,character-array done , working on 1-18

---
+++ 1-17/main.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define MAXLINE 1000
+
+int getline(char s[] , int limit);
+void copy(char to[] , char from[]);
+
+int main()
+{
+    int len;
+    char line[MAXLINE];
+    char longest[MAXLINE];
+    while((len = getline(line, MAXLINE))>0){
+        if(len>80){
+
+        copy(longest , line);
+    }
+
+    printf("%s" , longest);
+
+   return 0;
+}
+}
+
+int getline(char s[] , int lim){
+ int c , i;
+ for(i=0;i<lim-1 && (c=getchar())!=EOF && c!='\n';++i){
+    s[i]=c;
+ }
+ if(c=='\n'){
+    s[i]=c;
+    ++i;
+ }
+ s[i]='\0';
+return i;
+}
+
+void copy(char to[] , char from[]){
+int i=0;
+while((to[i]=from[i])!='\0')
+    ++i;
+
+}
